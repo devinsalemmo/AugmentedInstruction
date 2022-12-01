afb7b38df523bea1762a8a6b85b001ec60891df2
devinsalemmo <devinsal@buffalo.edu>
2022-12-01 12:06:04 -0500

    Object Detector Troubleshoot

--- app/src/main/java/com/example/finalarapp/CameraPage2.java
+++ app/src/main/java/com/example/finalarapp/CameraPage2.java
@@ -9,17 +9,30 @@ import androidx.camera.view.PreviewView;
 import androidx.core.content.ContextCompat;
 import androidx.lifecycle.LifecycleOwner;
 
+import android.content.res.AssetFileDescriptor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.Window;
 
 import com.google.common.util.concurrent.ListenableFuture;
 
+
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
+
+
 
 public class CameraPage2 extends AppCompatActivity {
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
     PreviewView previewView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +42,8 @@ public class CameraPage2 extends AppCompatActivity {
         getSupportActionBar().hide();
 
         setContentView(R.layout.activity_camera_page2);
+
+
 
         previewView = findViewById(R.id.cameraView);
 
@@ -44,7 +59,20 @@ public class CameraPage2 extends AppCompatActivity {
             }
 
         }, getExecutor());
+
+
     }
+
+    private MappedByteBuffer loadModelFile() throws IOException {
+        AssetFileDescriptor fileDescriptor=this.getAssets().openFd("app_model_v2.tflite");
+        FileInputStream inputStream=new FileInputStream(fileDescriptor.getFileDescriptor());
+        FileChannel fileChannel=inputStream.getChannel();
+        long startOffset=fileDescriptor.getStartOffset();
+        long declareLength=fileDescriptor.getDeclaredLength();
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY,startOffset,declareLength);
+    }
+
+
 
     private Executor getExecutor() {
         return ContextCompat.getMainExecutor(this);
--- app/src/main/java/com/example/finalarapp/MainActivity.java
+++ app/src/main/java/com/example/finalarapp/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.finalarapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.view.Window;
 
+
+
+
+import org.tensorflow.lite.support.image.TensorImage;
+import org.tensorflow.lite.task.core.BaseOptions;
+import org.tensorflow.lite.task.vision.detector.Detection;
+import org.tensorflow.lite.task.vision.detector.ObjectDetector;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+
+
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +40,68 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 
         setContentView(R.layout.activity_main);
+
+        //Object Detector options
+        ObjectDetector.ObjectDetectorOptions options =
+                ObjectDetector.ObjectDetectorOptions.builder()
+                        .setBaseOptions(BaseOptions.builder().useGpu().build())
+                        .setMaxResults(1)
+                        .build();
+
+
+        //Open file from asset folder and read/write it to new file thats accessible on emulator
+        File model1 = Environment.getExternalStorageDirectory();
+        File dir = new File(model1.getAbsolutePath() + "/Documents/ARApp");
+        dir.mkdirs();
+
+        File model4 = new File(dir, "newmodel.tflite");
+        try {
+            model4.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            InputStream mngr = getAssets().open("app_model_v2.tflite");
+            FileOutputStream outputStream = new FileOutputStream(model4, false);
+            int read;
+            byte[] bytes = new byte[mngr.available()];
+            while ((read = mngr.read(bytes)) != -1){
+                outputStream.write(bytes, 0, read);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //Create Object Detector and Run Test Image
+        try {
+            ObjectDetector objectDetector = ObjectDetector.createFromFileAndOptions(model4, options);
+            Bitmap input = BitmapFactory.decodeResource(getResources(), R.mipmap.modeltest1); //Converts manual image to bitmmap for input
+            TensorImage image = TensorImage.fromBitmap(input);
+            List<Detection> results = objectDetector.detect(image);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+
+
         //Creates Button to leave homepage
-        Button homeButton = (Button)findViewById((R.id.home_page_button));
+        Button homeButton = (Button) findViewById((R.id.home_page_button));
         homeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, TopicsPage.class));
             }
         });
-        }
+    }
+
+
+
+
+
     }
